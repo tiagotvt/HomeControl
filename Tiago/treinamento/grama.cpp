e05ea9c5db69545396f68441d2927c2961ed72c1
tiagotvt <tico.tvt@gmail.com>
2016-10-12 03:43:48 -0300

    segunda versão com treinamento quase pronto e sprinkler funcionando

--- Tiago/treinamento/grama.cpp
+++ Tiago/treinamento/grama.cpp
@@ -9,7 +9,6 @@
 
 using namespace cv;
 using namespace std;
-#define PI 3.14159265
 struct range {
   Scalar min;
   Scalar max;
@@ -27,68 +26,7 @@ struct mapBlock {
   int block_numX;
   int block_numY;
 };
-
-struct sprinkler {
-  int posX;
-  int posY;
-  int range;
-  int angle;
-  int orientation;
-};
-
 vector<Point> userPoints;
-
-float dist (sprinkler sp, int x, int y, int blockSize)
-{
-  float posx=0, posy=0, difX, difY;
-  if(x == 5)
-    posx += (x*blockSize)+(blockSize/10);
-  else posx += (x*blockSize)+(blockSize/2);
-  posy += (y*blockSize)+(blockSize/2);
-  difX = abs(posx-sp.posX);
-  difY = abs(posy-sp.posY);
-
-  return (sqrt(pow(difY,2)+pow(difX,2)));
-}
-
-int calc_angle(sprinkler sp, int x, int y, int blockSize)
-{
-  int posx=0, posy=0, difX, difY;
-  posx += (x*blockSize)+(blockSize/2);
-  posy += (y*blockSize)+(blockSize/2);
-  difX = posx-sp.posX;
-  difY = posy-sp.posY;
-  switch(sp.orientation)
-  {
-    case 0:
-      if(difX < 0)
-      {
-        return (atan(abs(difX/difY))*(180/PI))+90;
-      }
-      else return 90-(atan(abs(difX/difY))*(180/PI));
-    
-    case 1:
-      if(difY > 0)
-      {
-        return (atan(abs(difY/difX))*(180/PI))+90;
-      }
-      else return 90-(atan(abs(difX/difY))*(180/PI));
-
-    case 2:
-      if(difY < 0)
-      {
-        return (atan(abs(difY/difX))*(180/PI))+90;
-      }
-      else return 90-(atan(abs(difX/difY))*(180/PI));
-
-    case 3:
-      if(difX > 0)
-      {
-        return (atan(abs(difX/difY))*(180/PI))+90;
-      }
-      else return 90-(atan(abs(difX/difY))*(180/PI));
-  } 
-}
 
 void CallBackFunc (int event, int x, int y, int flags, void* userdata)
 {
@@ -209,17 +147,7 @@ int main( int argc, char** argv )
   //good.max = Scalar(80, 255, 255);
   ipfile.close();
 
-
-  int tmp;
-  vector<sprinkler> sprinklerVec;
-  ifstream spfile("sprinkler.txt");
-
-  for(tmp = 0; tmp < 6; tmp++)
-  {
-    sprinklerVec.push_back(sprinkler());
-    spfile >> sprinklerVec[tmp].posX>>sprinklerVec[tmp].posY>>sprinklerVec[tmp].range>>sprinklerVec[tmp].angle>>sprinklerVec[tmp].orientation;
-  }
-  spfile.close();
+  
 
   Mat edge,dst,threshold, final;
   Mat HSV, RGB;
@@ -246,7 +174,7 @@ int main( int argc, char** argv )
 
   int minBound=dst.rows;
   int maxBound=0;
-  tmp=0;
+  int tmp=0;
 
   for(int i = 0; i < dst.cols; i++){  
      for(int j = 0; j < dst.rows; j++){
@@ -285,7 +213,7 @@ int main( int argc, char** argv )
      {
       for(x_cont = (sqr*sqr_num_x); x_cont < (sqr*(sqr_num_x+1)); x_cont++) 
       {
-          if ( final.at<Vec3b>(y_cont, x_cont) != Vec3b(0,0,0) && final.at<Vec3b>(y_cont, x_cont) != Vec3b(255,255,255) ) 
+          if ( final.at<Vec3b>(y_cont, x_cont) != Vec3b(0,0,0) ) 
           {
             blue_temp += final.at<Vec3b>(y_cont, x_cont)[0];
             green_temp += final.at<Vec3b>(y_cont, x_cont)[1];
@@ -326,13 +254,13 @@ int main( int argc, char** argv )
     printf("%d  \n",sqr_num_x);
     printf("TESTEEE3\n");
   }
-
 /*******Testing*******/
   imwrite( "sqr.jpg", final_sqr );
+  //cvtColor(final,RGB,CV_HSV2BGR);
   RGB = imread( "sqr.jpg");
   Mat mask,mask2;
+  //cvtColor(final_sqr,RGB,CV_HSV2BGR);
   inRange(RGB,good.min,good.max,mask);
-
   // Copia todos os pixels da imagem original que não são pretos no threshold
   final_sqr.copyTo(mask2, mask);
   vector<mapBlock> blockMap;
@@ -348,7 +276,7 @@ int main( int argc, char** argv )
   }
 
   makeGrid(RGB, blockSize);
-  namedWindow ("Display Grid", WINDOW_FREERATIO);
+  namedWindow ("Display Grid", WINDOW_NORMAL);
   imshow("Display Grid", RGB);
   setMouseCallback("Display Grid", CallBackFunc, 0);
   waitKey(0);
@@ -376,13 +304,8 @@ int main( int argc, char** argv )
   {
     cout << blockError[tmp].x << " " << blockError[tmp].y <<"\n";
   }
-
-  
-  for(tmp=0; tmp < sprinklerVec.size(); tmp++)
-  {
-    circle(RGB, Point(sprinklerVec[tmp].posX,sprinklerVec[tmp].posY), 10, Scalar(0,255,255),-1,8);
-    cout << sprinklerVec[tmp].posX<<" "<<sprinklerVec[tmp].posY<<" "<<sprinklerVec[tmp].range<<" "<<sprinklerVec[tmp].angle<<" "<<sprinklerVec[tmp].orientation<<"\n";
-  }
+//for(auto tmp = userdata.begin(); tmp != userdata.end(); ++tmp)
+//  cout << userdata << "\n";
 
 //printf("%f %f %f\n",good.min[0],good.min[1],good.min[2]);
   ofstream ipfile2;
@@ -390,64 +313,8 @@ int main( int argc, char** argv )
   ipfile2 << good.min[0]<< " " << good.min[1] << " " << good.min[2]<< "\n";
   ipfile2 << good.max[0]<< " " << good.max[1] << " " << good.max[2];
   ipfile2.close();
+
 /********END TESTING************/
-
-/********CHOOSE SPRINKLER*******/
-vector<block> blockSprinkler;
-  ofstream ipfile3;
-  ipfile3.open("water.txt");
-  
-
-for(tmp=0; tmp < blockMap.size(); tmp++)
-{
-  blockTemp.x = (blockMap[tmp].block_numX);
-  blockTemp.y = (blockMap[tmp].block_numY); 
-
-  for(tmp2=0; tmp2 < blockSprinkler.size(); tmp2++)
-  {
-    if((blockTemp.x == blockSprinkler[tmp2].x) && (blockTemp.y == blockSprinkler[tmp2].y))
-      notSameBlock = false;
-  }
-  if(notSameBlock)
-    blockSprinkler.push_back(blockTemp);
-
-  notSameBlock = true;
-}
-
-  for(tmp=0; tmp < blockSprinkler.size(); tmp++)
-  {
-    cout << blockSprinkler[tmp].x << " " << blockSprinkler[tmp].y <<"\n";
-  }
-
-  float hipo = 0, new_hipo = 0;
-  int sp_num;
-  for(tmp=0; tmp < blockSprinkler.size(); tmp++)
-  {
-    sp_num = 0;
-    printf("BLOCK %d  %d\n",blockSprinkler[tmp].x, blockSprinkler[tmp].y );
-    for(tmp2=0; tmp2 < sprinklerVec.size(); tmp2++)
-    {
-      new_hipo = dist (sprinklerVec[tmp2], blockSprinkler[tmp].x, blockSprinkler[tmp].y, blockSize);
-      printf("%f \n", new_hipo);
-      if(new_hipo < hipo)
-      {
-        printf("%d -> %d \n",sp_num, tmp2);
-        sp_num = tmp2;
-        hipo = new_hipo;
-      }
-    }
-    hipo = 4000.0;
-    if(blockSprinkler[tmp].x == 5)
-    {
-      line(RGB, Point((blockSprinkler[tmp].x*blockSize)+(sqr/2), (blockSprinkler[tmp].y*blockSize)+(blockSize/2)), Point(sprinklerVec[sp_num].posX, sprinklerVec[sp_num].posY), Scalar(0, 255, 255),2);
-      cout <<"irraaaaaaa";
-    }
-    else line(RGB, Point((blockSprinkler[tmp].x*blockSize)+(blockSize/2), (blockSprinkler[tmp].y*blockSize)+(blockSize/2)), Point(sprinklerVec[sp_num].posX, sprinklerVec[sp_num].posY), Scalar(0, 255, 255),2);
-    ipfile3 << sp_num << " " << calc_angle(sprinklerVec[sp_num], blockSprinkler[tmp].x, blockSprinkler[tmp].y, blockSize) << "\n";
-  }
-
-  ipfile3.close();
-/******END CHOOSING SPRINKLER***/
   imwrite( "sqr.jpg", final_sqr );
   imwrite( "rgb.jpg", RGB );
   imwrite( "MASK.jpg", mask );
