f72e6d8f742a3c734a403f1603d6e45d71e08a2d
tiagotvt <tico.tvt@gmail.com>
2016-11-06 01:54:22 -0200

    Add files via upload

---
+++ Tiago/Arduino/ComLed.ino
@@ -0,0 +1,256 @@
+/*
+O comando AT disconecta o bluetooth case ele esteja conectado
+Limpar o buffer serial antes de checar mensagem >> mySerial.flush()
+
+*/
+#include <PWMServo.h>
+#include <SoftwareSerial.h>
+#include "packet.h"
+
+//SERVO
+int pos = 7;
+int servo = 9;
+PWMServo myservo;  // create servo object to control a servo
+
+int inPin = 12;   // choose the input pin (for a pushbutton)
+int val = 0;     // variable for reading the pin status
+int flag = 0;
+
+//LEDS
+int redPin = 11;
+int greenPin = 10;
+int bluePin = 6;
+//MAC ADDRESS
+String master = "A4D57812B725";
+String yellow = "78A5048C47AF";
+String green = "A4D57869D3D0";
+//Sensor Umidade
+const int pin = A0;
+int umidade = 0;
+int angulo = 0;
+//SoftwareSerial mySerial(11, 10); // RX, TX
+const int botao1 = 8;     // the number of the pushbutton pin
+const int botao2 = 7;
+const int botao3 = 6; 
+const int botao4 = 5;
+int ROLE = 1;    //variavel que armazena o estado do bluetooth
+String MAC = master;
+String stin = "";
+String stout = "";
+int i = 0;
+int conn;
+char sum;
+char cs;
+int agua;
+int trigger = 0;
+
+packet pkin, pkout;
+
+void setup()
+{
+  //SERVO
+  myservo.attach(9);
+  pinMode(13, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(inPin, INPUT);
+  digitalWrite(13, HIGH);
+  myservo.write(pos);
+  //SET THE LEDs
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+}
+
+  // set the data rate for the SoftwareSerial port
+  mySerial.begin(9600);
+
+  mySerial.print("AT+ROLE0");
+  delay(800);
+  //MAC = "AT+CON" + MAC;
+  mySerial.print("AT+CONA4D57812B725");
+}
+
+//------------------------------------------------------LOOP------------------------------------------------------------
+
+void loop() // run over and over
+{
+      val = digitalRead(inPin);  // read input value
+      if (val == HIGH) {         // check if the input is HIGH (button released)
+                if(flag == 1)
+                {
+                flag = 0;
+                setColor(0, 0, 0);
+                Serial.println("DESLIGANDO...");
+                delay(10000);
+                }
+      }
+        while(flag == 0)
+        {
+              val = digitalRead(inPin);  // read input value
+              if (val == HIGH) {         // check if the input is HIGH (button released)
+                Serial.println("LIGADO!");
+                flag = 1;
+                setColor(128, 255, 0);
+                delay(10000);
+              } else {
+                Serial.println("OFF!");
+                setColor(255, 0, 0);
+                delay(1000);
+              }
+        }
+/*
+//conecta
+      if (botao1 == 1)  //verifica o estado do botao e executa se ele estiver pressionado
+      {
+          Serial.write("\n\nconectando\n");  //avisa que a rotina foi iniciada
+          conn = connect(master);
+          if (conn == 1) Serial.print("conectou\n");
+          else
+          { 
+          if (conn == 0) Serial.print("erro\n");
+          if (conn == -1) Serial.print("erro desconhecido\n");
+          }
+      }
+
+//desconecta
+      if (botao2 == 1)  //verifica o estado do botao e executa se ele estiver pressionado
+      {
+          Serial.write("\n\ndesconectando\n");  //avisa que a rotina foi iniciada
+          conn = disconnect();
+          if (conn == 0) Serial.write("desconectado");        
+          else Serial.write("Ja estava desconectado");
+      } 
+     
+//envia
+      if (botao3 == 1)  //verifica o estado do botao e executa se ele estiver pressionado
+      {
+              Serial.write("\nenviando\n");
+              send(yellow, "teste");
+            //mySerial.write("1");
+      }
+
+//recebe
+      if (botao4 == 1)  //verifica o estado do botao e executa se ele estiver pressionado
+      {
+              Serial.write("\nrecebendo\n");
+              getpkt();
+      }
+*/
+//loop para passar as mensagens entre o serial virtual e o TX RX (serve para permitir que voce mande comandos ao modulo pelo terminal)    
+/*      if (mySerial.available())
+          Serial.write(mySerial.read());
+      if (Serial.available())
+          mySerial.write(Serial.read());
+*/
+
+//leitura do sensor de agua
+
+      mySerial.flush();
+      Serial.flush();
+      delay(100);
+//loop para esperar solicitacao do master
+            if (mySerial.available())
+            {
+              
+              pkin.data = getstr();
+              delay(100);
+              
+              if (pkin.data == "OK+CONN")
+                conn = 1;
+              if (pkin.data == "OK+LOST")
+                disconnect();
+                conn = 0;
+              if (pkin.data.substring(0, 4) == "agua")
+              {
+                delay(800);
+                angulo = pkin.data.substring(4,7).toInt();
+                Serial.println(pkin.data.substring(4,7));
+                //mySerial.write("AT+ROLE1");  //coloca o modulo em modo master para se conectar a outro
+                delay(800);
+                for(i = 0;i<10;i++){
+                  delay(100);
+                  umidade += analogRead(pin);
+                }
+                umidade = umidade/10;
+                umidade = map(umidade, 476, 1023, 100, 0);
+                //Serial.print("Umidade media mapeada = ");
+                Serial.println(umidade);
+                delay(100);
+                
+                //mySerial.print("AT+CONA4D57812B725");
+                delay(100);
+                //mySerial.write(umidade);
+                stout = umidade;
+                
+                delay(500);
+                if(umidade < 30){
+                  pos=angulo;
+                  //pos = map(angulo, 0, 180, 7, 170);
+                  myservo.write(pos);
+                  Serial.print("Umidade de: ");
+                  Serial.println(umidade);
+                  Serial.println("Preparando para irrigacao");
+                  Serial.print("Sprinkler com angulo: ");
+                  Serial.println(angulo);
+                  delay(2000);
+                  Serial.println("INICIO DA IRRIGACAO");
+                  digitalWrite(3,HIGH);
+                  setColor(80, 0, 80); // purple
+                  Serial.print("[");
+                  for(i=0; i < 10; i++)
+                  {
+                    Serial.print("..");
+                    delay(1000);
+                  }
+                  Serial.print("]\n");
+                  Serial.println("FIM DA IRRIGACAO");
+                  digitalWrite(3,LOW);
+                }
+                if(umidade > 30){
+                  Serial.print("Umidade de: ");
+                  Serial.println(umidade);
+                  Serial.println("NAO PRECISA SER MOLHADA");
+                  setColor(0, 255, 0); // green
+                  delay(10000);
+                }
+                //mySerial.write("AT+ROLE0");
+                //send(master, stout);
+                umidade = 0;
+              }
+              Serial.println("ON");
+              setColor(128, 255, 0);
+            }
+            delay(1000);
+
+}
+
+//------------------------------------------------------END LOOP---------------------------------------------------------------
+void setColor(int red, int green, int blue)
+{
+#ifdef COMMON_ANODE
+red = 255 - red;
+green = 255 - green;
+blue = 255 - blue;
+#endif
+analogWrite(redPin, red);
+analogWrite(greenPin, green);
+analogWrite(bluePin, blue);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Tiago/Arduino/packet.h
@@ -0,0 +1,264 @@
+#include <SoftwareSerial.h>
+#include <Arduino.h>
+SoftwareSerial mySerial(7, 8); // RX, TX
+
+struct packet
+{
+  String ids;
+  String idr;
+  String data;
+  char cs;
+};
+
+//function prototypes
+packet getpkt(void);
+char checksum(String);
+int send(String, String);
+String getstr(void);
+int connect(String);
+int disconnect(void);
+int bounce(packet);
+
+const packet errorpkt = {"000000000000", "000000000000", "error", 0};
+
+packet pkt;
+String check = "";
+
+String idlocal = "A4D57869D3D0";
+
+
+//------------------------------------------------------CHECKSUM---------------------------------------------------------------
+
+char checksum(String data)
+{
+  char checksum = 0;
+  int i = 0;
+  while (i < data.length())
+  {
+    checksum = checksum + data.charAt(i);
+    i++;
+  }
+  return (checksum);
+}
+
+//------------------------------------------------------END CHECKSUM-----------------------------------------------------------
+//------------------------------------------------------SEND-------------------------------------------------------------------
+
+int send(String idr, String data)
+{
+  //colocar o mac do modulo que esta sendo gravado aqui
+  //String ids = "78A5048C4A04";  //green
+  //String ids = "78A5048C47AF";  //yellow
+
+  packet temp;
+  String buff = "";
+  char cs = checksum(data);
+  buff = idlocal + idr + data + cs;
+
+  for (int i = 0; i < 3; i++)
+  {
+    mySerial.print(buff);
+    if (data == "CSOK") return (0); //excecao para impedir um loop infinito de confirmacao
+    if (data == "CSFAIL") return (0); //excecao para impedir um loop infinito de confirmacao
+    temp = getpkt();
+    if (temp.data == "CSOK") return (0);
+  }
+  return (1);
+}
+
+//------------------------------------------------------END SEND---------------------------------------------------------------
+//------------------------------------------------------GETPACKET--------------------------------------------------------------
+
+packet getpkt()
+{
+  char c;
+  String buff = "";
+  packet ret;
+  int l;
+  int i = 0;
+
+  //mySerial.flush();
+
+  for (int z = 0; z < 3; z++)
+  {
+    while (!mySerial.available() && i < 1000 ) //espera a informacao comecar a chegar. Se demorar mais de 10s cancela
+    {
+      i++;
+      delay(10);
+    }
+
+    if (i == 1000)
+    {
+      Serial.write("sem dados no serial\n");
+      return (errorpkt);
+    }
+
+    while (mySerial.available() > 0)  //transfere o buffer do mySerial para a string buff
+    {
+      c = mySerial.read();
+      buff += c;
+      delay(10);  //sem esse delay o while acaba antes de todos os dados passarem
+    }
+    Serial.print(buff); //debug
+
+    l = buff.length();
+    if (l<24) 
+    {
+      ret = errorpkt;
+      ret.data = buff;
+      return(ret);
+    }
+    ret.ids = buff.substring(0, 12);
+    ret.idr = buff.substring(12, 24);
+    ret.data = buff.substring(24, l - 1);
+    ret.cs = buff.charAt(l - 1);
+
+    if (ret.data == "CSOK") return (ret); //excecao para impedir um loop infinito de confirmacao
+    if (ret.data == "CSFAIL") return (ret); //excecao para impedir um loop infinito de confirmacao
+    if (ret.idr != idlocal) //definir o MAC do módulo que está sendo gravado!
+    {
+       //l = bounce(ret);
+       if (l == 0) return(errorpkt);
+    }
+
+    if (ret.cs == checksum(ret.data))
+    {
+      send(ret.ids, "CSOK");
+      Serial.write("OK!\n"); //debug
+      return (ret);
+    }
+    else send(ret.ids, "CSFAIL");
+  }
+  return (errorpkt);
+}
+
+//------------------------------------------------------END GETPACKET----------------------------------------------------------
+//------------------------------------------------------GETSTRING--------------------------------------------------------------
+String getstr()
+{
+  char c;
+  String buff = "";
+  int i = 0;
+
+  //mySerial.flush();
+
+  while (!mySerial.available() && i < 1000 ) //espera a informacao comecar a chegar. Se demorar mais de 10s cancela
+  {
+    i++;
+    delay(10);
+  }
+
+  if (i == 1000)
+  {
+    Serial.write("sem dados no serial\n");
+    return ("0");
+  }
+
+  while (mySerial.available() > 0)  //transfere o buffer do mySerial para a string buff
+  {
+    c = mySerial.read();
+    buff += c;
+    delay(10);  //sem esse delay o while acaba antes de todos os dados passarem
+  }
+
+  Serial.print(buff); //debug
+  Serial.print("\n"); //debug
+
+  return (buff);
+}
+
+//------------------------------------------------------END GETSTRING----------------------------------------------------------
+//------------------------------------------------------CONNECT----------------------------------------------------------------
+int connect(String MAC)
+{
+  mySerial.write("AT+ROLE1");  //coloca o modulo em modo master para se conectar a outro
+  delay(800);
+  pkt = getpkt();
+
+  int err;
+  MAC = "AT+CON" + MAC;
+  for (err = 0; err < 3; err++)
+  {
+    mySerial.print(MAC);  //conecta com o modulo especificado pelo mac
+
+//    Serial.print(MAC);
+    pkt = getpkt();
+
+    if (pkt.data == "OK+CONNA") //se verificado que o módulo entendeu o comando, limpa a string e prossegue
+    {
+      pkt.data.remove(0); //se o ble entendeu o comando, limpa a string e prossegue
+    }
+    else return (-1); //se nao, retornar erro desconhecido
+
+    pkt = getpkt();
+
+    if (pkt.data == "OK+CONN")    //verifica se a conexao foi efetuada
+    {
+      pkt.data.remove(0);
+      return (0);
+    }
+  }
+
+  pkt.data.remove(0);
+  return (-2);
+
+}
+
+//------------------------------------------------------END CONNECT------------------------------------------------------------
+//------------------------------------------------------DISCONNECT-------------------------------------------------------------
+
+int disconnect()
+{
+  mySerial.write("AT");  //Se o modulo estiver conectado, AT termina a conexao
+
+  check = getstr();
+
+  if (check == "OK+LOST")
+  {
+    delay(500);
+    mySerial.write("AT+ROLE0");  //coloca o modulo em modo slave para ficar disponivel a receber conexoes
+    check = getstr();
+    return (0);
+  }
+  else if (check == "OK")
+  {
+    delay(500);
+    check.remove(0);
+    mySerial.write("AT+ROLE0");  //coloca o modulo em modo slave para ficar disponivel a receber conexoes
+    check = getstr();
+    return (-1);
+  }
+  return(-2);
+}
+
+//------------------------------------------------------END DISCONNECT---------------------------------------------------------
+
+//------------------------------------------------------BOUNCE-------------------------------------------------------------
+int bounce(packet foward)
+{
+  packet reply;
+
+  Serial.write("\nmark 1\n");
+  disconnect();
+  Serial.write("\nmark 2\n");
+  connect(foward.idr);
+  Serial.write("\nmark 3\n");
+  send(foward.idr, foward.data);
+  Serial.write("\nmark 4\n");
+  reply = getpkt();
+  Serial.write("\nmark 5\n");
+  delay(500);
+  disconnect();
+  Serial.write("\nmark 6\n");
+  connect(foward.ids);
+  Serial.write("\nmark 7\n");
+  delay(2000);  //DIMINUIR ESSE DELAY - ESTA ALTO PARA PERMITIR QUE A RASP SEJA COLOCADA EM GETPKT MANUALMENTE
+  send(foward.ids,reply.data);
+  Serial.write("\nmark 8\n");
+  return(0);
+}
+
+//------------------------------------------------------END BOUNCE-------------------------------------------------------------
+
+
+
